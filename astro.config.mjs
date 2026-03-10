9cbb5da3aa1aeb022e11c816bc59c1f0966853bd
vaidehi gohil <v.dahi@vaidehis-MacBook-Air.local>
2026-03-10 16:52:04 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,7 @@
+// DEPLOY COMMANDS:
+// git add . && git commit -m "update" && git push origin main
+// npm run deploy
+
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
